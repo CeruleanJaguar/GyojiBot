4aa43e0a84d7bcca225bb3e64a395abbb0bc9678
CeruleanJaguar <salomdi@gmail.com>
2020-05-03 10:52:08 +0100

    Add help text to `submit_slohyo` command.

--- gyoji/commands/slohyo.py
+++ gyoji/commands/slohyo.py
@@ -9,7 +9,11 @@ class Slohyo(commands.Cog):
     
     # Commands
     
-    @commands.command(name='Submit Slohyo', aliases=['allow me to submit a slohyo'])
+    @commands.command(
+        name='Submit Slohyo',
+        aliases=['allow me to submit a slohyo'],
+        description='Submit a Slohyo form in discord. Requires you to mention your opponent. GyojiBot will direct message you to complete the form.'
+    )
     async def submit_slohyo(self, ctx):
         author = ctx.author
         opponent = 'nil'
